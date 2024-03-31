6bb6425d34b21774db3f375f8f7e65c9de394e9a
Eugene_dev <lointainyyy@gmail.com>
2024-03-31 22:15:16 +0300

    added new name for file, fix path

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const { login, register, verifyEmail } = require('../controllers/userController');
+const { login, register, verifyEmail } = require('../controllers/authController');
 
 router.post('/login', login);
 router.post('/register', register);
