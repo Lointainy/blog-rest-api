5969185fcbb3dd6a8cf37b974fda410808c5bf2e
Eugene_dev <lointainyyy@gmail.com>
2024-03-31 23:54:22 +0300

    added auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const { getUserById } = require('../data/user');
+
+const authMiddleware = async (req, res, next) => {
+	const { authorization } = req.headers;
+
+	if (!authorization) {
+		return res.status(401).json({ error: 'errorAuthToken' });
+	}
+
+	const token = authorization.split(' ')[1];
+
+	try {
+		const { id } = jwt.verify(token, process.env.JWT_SECRET);
+
+		req.user = await getUserById(id);
+
+		next();
+	} catch (error) {
+		return res.status(401).json({ error: 'errorAuth' });
+	}
+};
+
+module.exports = authMiddleware;
+
